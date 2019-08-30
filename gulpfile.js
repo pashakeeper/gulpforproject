d7d399b4dd4e0af5ba21f49f658bc361e0f6ab6f
pashakeeper <pashagkjunost@gmail.com>
2019-08-30 14:28:28 +0300

    IN process

--- gulpfile.js
+++ gulpfile.js
@@ -24,11 +24,10 @@ function watch() {
 }
 
 function style() {
-    return gulp.src('./src/sass/**/*.scss')
+    return gulp.src('./src/sass/*.scss')
         .pipe(sass())
         .pipe(autoprefixer(['last 25 versions', '> 1%', 'ie 8', 'ie 7', 'ie 10', 'ie 9', 'ie 11'],
             {cascade: false}))
-        .pipe(cssnano())
         .pipe(gulp.dest('./src/css'))
         .pipe(browserSync.stream(true))
 
@@ -42,7 +41,7 @@ function libs() {
         .pipe(cssnano())
         .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('./src/css'))
-        .pipe(browserSync.stream(true))
+        // .pipe(browserSync.stream(true))
 }
 
 
@@ -50,16 +49,42 @@ function clean() {
     return del(['./dist/'])
 }
 
+function buildImg() {
+    return gulp.src('./src/img/**/*')
+        .pipe(gulp.dest('./dist/img'))
+}
+function buildFont() {
+    return gulp.src('./src/fonts/**/*')
+        .pipe(gulp.dest('./dist/fonts'))
+}
+function buildCss() {
+    return gulp.src('./src/css/**/*.css')
+        .pipe(cssnano())
+        .pipe(gulp.dest('./dist/css'))
+
+}
+function buildScripts() {
+    return gulp.src('./src/js/**/*.js')
+        .pipe(gulp.dest('./dist/js'))
+}
+function buildHtml() {
+    return gulp.src('./src/**.html')
+        .pipe(gulp.dest('./dist'))
+}
+
 function scriptslibs() {
     return gulp.src([
         './src/libs/jquery/dist/jquery.min.js',
         './src/libs/bootstrap4/dist/js/bootstrap.min.js',
+        './src/libs/owl.carousel/dist/owl.carousel.min.js',
     ])
         .pipe(concat('libs.min.js'))
         .pipe(uglify())
         .pipe(gulp.dest('src/js'))
-        .pipe(browserSync.stream(true))
+        // .pipe(browserSync.stream(true))
 }
+
+const build = gulp.series(clean, gulp.parallel(buildImg, buildFont, buildCss, buildScripts,buildHtml));
 
 
 
@@ -68,4 +93,5 @@ gulp.task('style', style);
 gulp.task('libs', libs);
 gulp.task('scriptslibs', scriptslibs);
 gulp.task('clean', clean);
-gulp.task('default', watch);
+gulp.task('watch', watch);
+gulp.task('build',build);
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,3 +1,87 @@
 $(document).ready(function () {
+    $('.slider_banner').owlCarousel({
+        items: 1,
+        nav: true,
+        navText: [$('.prev'), $('.next')],
+    });
+    let brands = $('.brands_slider');
+    let popular = $('.popular_slider');
+    let newprod = $('.new_slider');
+    let stockslider = $('.stock_slider');
+    brands.owlCarousel({
+        items: 4,
+        nav: false,
+        dots: false
+    });
+    popular.owlCarousel({
+        items: 4,
+        nav: false,
+        dots: false
+    });
+    newprod.owlCarousel({
+       items: 4,
+       nav: false,
+       dots: false,
+    });
+    stockslider.owlCarousel({
+       items: 4,
+       nav: false,
+       dots: false,
+    });
+    $('.brands_navs .next_arr').click(function() {
+        brands.trigger('next.owl.carousel');
+    });
+    $('.brands_navs .prev_arr').click(function() {
+        brands.trigger('prev.owl.carousel');
+    });
+    $('.popular .next_arr').click(function() {
+        popular.trigger('next.owl.carousel');
+    });
+    $('.popular .prev_arr').click(function() {
+        popular.trigger('prev.owl.carousel');
+    });
+    $('.new .next_arr').click(function() {
+        newprod.trigger('next.owl.carousel');
+    });
+    $('.new .prev_arr').click(function() {
+        newprod.trigger('prev.owl.carousel');
+    });
+    $('.stock .next_arr').click(function() {
+        stockslider.trigger('next.owl.carousel');
+    });
+    $('.stock .prev_arr').click(function() {
+        stockslider.trigger('prev.owl.carousel');
+    });
+    $('<div class="quantity-nav"><div class="quantity-button quantity-down">-</div><div class="quantity-button quantity-up">+</div></div>').insertAfter('.quantity input');
+    $('.quantity').each(function() {
+        var spinner = jQuery(this),
+            input = spinner.find('input[type="number"]'),
+            btnUp = spinner.find('.quantity-up'),
+            btnDown = spinner.find('.quantity-down'),
+            min = input.attr('min'),
+            max = input.attr('max');
 
+        btnUp.click(function() {
+            var oldValue = parseFloat(input.val());
+            if (oldValue >= max) {
+                var newVal = oldValue;
+            } else {
+                var newVal = oldValue + 1;
+            }
+            spinner.find("input").val(newVal);
+            spinner.find("input").trigger("change");
+        });
+
+        btnDown.click(function() {
+            var oldValue = parseFloat(input.val());
+            if (oldValue <= min) {
+                var newVal = oldValue;
+            } else {
+                var newVal = oldValue - 1;
+            }
+            spinner.find("input").val(newVal);
+            spinner.find("input").trigger("change");
+        });
+
+    });
 });
